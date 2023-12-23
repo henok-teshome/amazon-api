754e8a8e859907fc3fb7aeeba11ecc49335ecac6
Henok <henok_206@yahoo.com>
2023-12-23 16:35:47 +0300

    update packajson

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,39 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+/* eslint-disable max-len */
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+    "sk_test_51OPPwVFIMHZ2ryul7S6PmN4IFvzb4IMhlSlUVuG5DAgVfdmsqUY6CMAjEdivp9toqIskHk7vGmWWDGSUvBIst5QA001ollhNqN"
+);
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// App config
+const app = express();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Routes
+app.get("/", (request, response) => {
+    response.status(200).send("Hello World");
+});
+
+app.post("/payments/create", async (request, response) => {
+    const total = request.query.total;
+    console.log("Payment Request Received for this amount >>>>>>>>", total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "USD",
+    });
+
+    // Created
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
+
+// Listen command
+exports.api = functions.https.onRequest(app);
+
+//  base url http://127.0.0.1:5001/dec-22edb/us-central1/api
